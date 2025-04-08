2a95b4a6aa9a041a7b15bfbccdf40d5b6aed3c71
Helik Thacker <thackerhelik@gmail.com>
2025-04-08 22:01:24 +0000

    Adding the frontend for the app

---
+++ octofit-tracker/frontend/src/components/Workouts.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+
+function Workouts() {
+  const [workouts, setWorkouts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://curly-sniffle-jj4jww4rw943qj5g-8000.app.github.dev/api/workouts/')
+      .then(response => response.json())
+      .then(data => setWorkouts(data))
+      .catch(error => console.error('Error fetching workouts:', error));
+  }, []);
+
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h1 className="card-title">Workouts</h1>
+        <ul className="list-group">
+          {workouts.map(workout => (
+            <li key={workout._id} className="list-group-item">{workout.name} - {workout.description}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Workouts;
